f1d018ea4fda7033d9a8e1f66e013fa1a0ad6c7c
banksis02 <81013510+banksis02@users.noreply.github.com>
2023-05-12 21:00:57 +0700

    Update kukukuy.lua

--- kukukuy.lua
+++ kukukuy.lua
@@ -4231,6 +4231,7 @@ function gameisFinishAuto()
 				wait(99)
 			end
 		else
+			pcall(function() webhook() end)
 			local a = { [1] = "replay" }
 			game:GetService("ReplicatedStorage").endpoints.client_to_server.set_game_finished_vote:InvokeServer(unpack(a))
 			game:GetService("ReplicatedStorage").endpoints.client_to_server.set_game_finished_vote:InvokeServer(unpack(a))
